bd6d41623d99539e458aaefd2d66ca47f929fda7
elilaiono <elilaiono@gmail.com>
2023-02-09 18:32:10 -0700

    fixed minor errors with enpoints requests

--- controllers/player_info.js
+++ controllers/player_info.js
@@ -18,13 +18,13 @@ const getAll = (req, res) => {
 };
 const getSingle = (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid contact id to find a contact.');
+    res.status(400).json('Must use a valid contact id to find a player.');
   }
   const userId = new ObjectId(req.params.id);
   mongodb
     .getDb()
     .db()
-    .collection('contacts')
+    .collection('player_info')
     .find({ _id: userId })
     .toArray((err, result) => {
       if (err) {
@@ -49,7 +49,7 @@ const createPlayer = async (req, res) => {
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    res.status(500).json(response.error || 'Some error occurred while creating the player.');
   }
 };
 
@@ -74,7 +74,7 @@ const updatePlayer = async (req, res) => {
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    res.status(500).json(response.error || 'Some error occurred while updating the player.');
   }
 };
 
@@ -83,12 +83,12 @@ const deletePlayer = async (req, res) => {
     res.status(400).json('Must use a valid contact id to delete a contact.');
   }
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('player_info').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    res.status(500).json(response.error || 'Some error occurred while deleting the player.');
   }
 };
 
